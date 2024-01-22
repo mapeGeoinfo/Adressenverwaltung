0770f23ad53a95c4f7a794b153683b69ccd10891
GEOINFO\MaPe <peter.mayrhofer1@geoinfo.ch>
2024-01-22 10:58:31 +0100

    Added Blank Application

---
+++ Adressverwaltung/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Windows;
+
+namespace Adressverwaltung
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+       
+
+        public App()
+        {
+         
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+            base.OnStartup(e);
+        }
+
+   
+
+    }
+}
